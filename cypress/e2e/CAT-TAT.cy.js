52c1e03627ec4cc73b9943cd488be09cf8623646
igoroliveiradasilva <@30igordeDEUS>
2025-12-18 00:32:47 -0300

    S3A10: exercício 1

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -1,9 +1,35 @@
 describe('Central de Atendimentos ao Cliente TAT', () => {
 
-    it('Verifica o título da aplicação', () => {
+    beforeEach(() => {
+        cy.viewport(470, 850) // ajuste igor
         cy.visit('./src/index.html')
+    });
 
+    it('Verifica o título da aplicação', () => {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    });
+
+    it('Preenche os campos obrigatórios e envia o formulário', () => {
+        // Nome
+        cy.get('[name="firstName"]')
+            .type('Carlos')
+        // Sobrenome
+        cy.get('[name="lastName"]')
+            .type('Oliveira')
+        // Email
+        cy.get(':nth-child(2) > :nth-child(1) > [name="email"]')
+            .type('carlos@dogmail.com')
+        // Textarea 'como podemos ajudar...'
+        cy.get('[name="open-text-area"]')
+            .type('Quero ração!')
+        // Enviar
+        cy.get('.button')
+            .click()
+
+        // Mensagem de sucesso
+        cy.get('.success > strong')
+            .should('be.visible')
+
     });
 
 });
