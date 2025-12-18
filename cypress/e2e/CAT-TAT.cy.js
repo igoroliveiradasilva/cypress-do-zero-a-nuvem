a1c3397e00cfd64697506a1fc1fdb17f5b47a96b
igoroliveiradasilva <@30igordeDEUS>
2025-12-17 23:19:29 -0300

    seção 2 completa

---
+++ cypress/e2e/CAT-TAT.cy.js
@@ -0,0 +1,9 @@
+describe('Central de Atendimentos ao Cliente TAT', () => {
+
+    it('Verifica o título da aplicação', () => {
+        cy.visit('./src/index.html')
+
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    });
+
+});
