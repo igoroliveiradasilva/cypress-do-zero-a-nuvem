80ed570c64afb4145350cbba0e28199cdbb32370
igoroliveiradasilva <@30igordeDEUS>
2025-12-21 22:48:07 -0300

    S3A18: exercício extra 7.3

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,12 @@
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit'), data => {
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'johndoe@example.com',
+    text: 'test'
+}) => {
     cy.get('[name="firstName"]').type(data.firstName)
     cy.get('[name="lastName"]').type(data.lastName)
     cy.get(':nth-child(2) > :nth-child(1) > [name="email"]').type(data.email)
     cy.get('[name="open-text-area"]').type(data.text)
     cy.get('.button').click()
-}
+})
