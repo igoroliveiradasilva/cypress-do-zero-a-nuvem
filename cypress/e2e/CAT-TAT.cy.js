15a6387c6fecdd70f9a2a4bfc7615fcf9aabda6a
igoroliveiradasilva <@30igordeDEUS>
2025-12-24 00:04:35 -0300

    S7A32: upload de arquivos com drag and drop

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -157,9 +157,17 @@ describe('Central de Atendimentos ao Cliente TAT', () => {
         cy.get('.error > strong').should('be.visible')
     });
 
-    it.only('Seleciona um arquivo da pasta fixtures', () => {
+    it('Seleciona um arquivo da pasta fixtures', () => {
         cy.get('#file-upload')
             .selectFile('cypress/cypress/fixtures/example.json')
+            .should(input => {
+                expect(input[0].files[0].name).to.equal('example.json')
+            })
+    });
+
+    it.only('Seleciona um arquivo simulando um drag-and-drop', () => {
+        cy.get('#file-upload')
+            .selectFile('cypress/cypress/fixtures/example.json', { action: 'drag-drop' })
             .should(input => {
                 expect(input[0].files[0].name).to.equal('example.json')
             })
