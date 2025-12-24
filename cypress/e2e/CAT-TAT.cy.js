a6e8277375ffaceb5135995c1f8bdcb17f62012a
igoroliveiradasilva <@30igordeDEUS>
2025-12-24 19:36:30 -0300

    S8A36: abrindo link na mesma aba

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -182,9 +182,17 @@ describe('Central de Atendimentos ao Cliente TAT', () => {
             })
     });
 
-    it.only('Verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    it('Verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
         cy.contains('a', 'Política de Privacidade')
             .should('have.attr', 'href', 'privacy.html')
             .and('have.attr', 'target', '_blank')
+    });
+
+    it.only('Acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+        cy.contains('a', 'Política de Privacidade')
+            .invoke('removeAttr', 'target')
+            .click()
+
+        cy.contains('h1', 'CAC TAT - Política de Privacidade')
     });
 });
