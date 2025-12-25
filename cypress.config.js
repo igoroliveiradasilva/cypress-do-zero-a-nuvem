876851ebf8a434afae68b17938a99b2f6f5b33d1
igoroliveiradasilva <@30igordeDEUS>
2025-12-25 20:15:55 -0300

    fix: aponta Cypress para estrutura do curso

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  e2e: {},
+  e2e: {
+    specPattern: "cypress/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/cypress/support/e2e.js",
+    fixturesFolder: "cypress/cypress/fixtures",
+  },
 }
