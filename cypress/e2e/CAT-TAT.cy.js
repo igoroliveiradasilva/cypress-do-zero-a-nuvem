b4b9ef759030860997d034e01ef6c47068f52a9a
igoroliveiradasilva <@30igordeDEUS>
2025-12-26 20:39:50 -0300

    Configure cypress cloud

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -159,7 +159,7 @@ describe('Central de Atendimentos ao Cliente TAT', () => {
 
     it('Seleciona um arquivo da pasta fixtures', () => {
         cy.get('#file-upload')
-            .selectFile('cypress/cypress/fixtures/example.json')
+            .selectFile('cypress/fixtures/example.json')
             .should(input => {
                 expect(input[0].files[0].name).to.equal('example.json')
             })
@@ -167,7 +167,7 @@ describe('Central de Atendimentos ao Cliente TAT', () => {
 
     it('Seleciona um arquivo simulando um drag-and-drop', () => {
         cy.get('#file-upload')
-            .selectFile('cypress/cypress/fixtures/example.json', { action: 'drag-drop' })
+            .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
             .should(input => {
                 expect(input[0].files[0].name).to.equal('example.json')
             })
