fa74010a85db52442f37bf13150f08b705224698
igoroliveiradasilva <@30igordeDEUS>
2025-12-25 21:07:39 -0300

    ajuste command 2

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    supportFile: "cypress/cypress/support/e2e.js",
+    supportFile: "cypress/support/e2e.js",
     fixturesFolder: "cypress/cypress/fixtures",
   },
 }
