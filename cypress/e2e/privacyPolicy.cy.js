255ae29fc1c3c04c5c6b53a9d74c44df6e2f0e54
igoroliveiradasilva <@30igordeDEUS>
2025-12-26 21:18:31 -0300

    S13A52: Lodash - times

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,6 +1,8 @@
-it('Testa página da política de privacidadee de forma independente', () => {
-    cy.visit('./src/privacy.html')
+Cypress._.times(3, () => {
+    it('Testa página da política de privacidadee de forma independente', () => {
+        cy.visit('./src/privacy.html')
 
-    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
-    cy.contains('p', 'Talking About Testing').should('be.visible')
-});
+        cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+        cy.contains('p', 'Talking About Testing').should('be.visible')
+    });
+})
